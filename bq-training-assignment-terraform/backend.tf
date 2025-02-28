852e77b64d0fe282c9c16e066a155a96f7156123
kanakkumrawat <108152018+kanakkumrawat@users.noreply.github.com>
2025-02-28 15:30:38 +0530

    Update backend.tf

--- bq-training-assignment-terraform/backend.tf
+++ bq-training-assignment-terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "bq-assignment"
+    use_lockfile   = true
   }
 }
 
